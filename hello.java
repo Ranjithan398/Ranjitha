2f7a1d3e7e4924294877ae9a77d772ef07a0e418
Ranjitha <ranjithan398@gmail.com>
2020-11-10 20:13:19 +0530

    save changes

--- hello.java
+++ hello.java
@@ -1,7 +1,6 @@
-public Small
+class hello{
+    public static void main(String[] args) 
 {
-public static void main(String args[])
-{
-System.out.println("Hello World");
-}
-}
+        System.out.println("Hello, World!"); 
+    }
+} 
